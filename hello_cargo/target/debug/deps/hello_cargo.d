357bb610656fe5710ea0846cacdd3012de0656ce
RaphaelNY <1278173058@qq.com>
2023-11-24 22:11:24 +0800

    init hello_cargo

---
+++ hello_cargo/target/debug/deps/hello_cargo.d
@@ -0,0 +1,5 @@
+E:\资料\Rust-Base-Learning\hello_cargo\target\debug\deps\hello_cargo.exe: src\main.rs
+
+E:\资料\Rust-Base-Learning\hello_cargo\target\debug\deps\hello_cargo.d: src\main.rs
+
+src\main.rs:
---
+++ hello_cargo/target/debug/hello_cargo.d
@@ -0,0 +1,1 @@
+E:\资料\Rust-Base-Learning\hello_cargo\target\debug\hello_cargo.exe: E:\资料\Rust-Base-Learning\hello_cargo\src\main.rs
