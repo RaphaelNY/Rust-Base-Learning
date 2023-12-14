31d9b9807cbad37c9f62bb6febe65bfb3b00a08a
RaphaelNY <1278173058@qq.com>
2023-12-14 11:40:50 +0800

    init struct_demo

---
+++ strct_demo/src/main.rs
@@ -0,0 +1,55 @@
+
+#[derive(Debug)] // this is a annotate
+struct Rectangle {
+    width: u32,
+    height: u32,
+}
+impl Rectangle {
+    fn rectangle(width: u32, height: u32) -> Rectangle {
+        Rectangle { width, height }
+    }
+
+    fn area(&self) -> u32 {
+        self.width * self.height
+    }
+}
+fn main() {
+    let width1 = 20;
+    let height1 = 30;
+    println!(
+        "The area of the rectangle is {} square pixels.",
+        area(width1, height1)
+    );
+
+    let dim = (20, 30);
+    println!(
+        "The area of the rectangle is {} square pixels.",
+        area2(dim)
+    );
+
+    let ect1 = Rectangle { width: 20, height: 30 };
+    let ect2 = Rectangle::rectangle(20, 30);
+    println!(
+        "The area of the rectangle is {} square pixels.",
+        area3(&ect1) // this is a borrowing of ect1
+    );
+    println!(
+        "The area of the rectangle is {} square pixels.",
+        ect2.area()
+    );
+
+    println!("ect1 is {:?} ", ect1);
+    println!("ect1 is {:#?}", ect1);
+}
+
+fn area(width: u32, height: u32) -> u32 {
+    width * height
+}
+
+fn area2(dimensions: (u32, u32)) -> u32 {
+    dimensions.0 * dimensions.1
+}
+
+fn area3(rectangle: &Rectangle) -> u32 {
+    rectangle.width * rectangle.height
+}
