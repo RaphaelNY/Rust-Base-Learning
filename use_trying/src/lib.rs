f485be805ab271dbc4220b04a68f63114504c130
RaphaelNY <1278173058@qq.com>
2023-12-17 23:58:37 +0800

    update

---
+++ use_trying/src/lib.rs
@@ -0,0 +1,18 @@
+mod front_of_house {
+    pub mod hosting {
+        pub fn add_to_waitlist() {}
+        fn some_function() {}
+    }
+}
+
+use crate::front_of_house::hosting;
+ // use self::front_of_house::hosting;
+ // use front_of_house::hosting;
+
+pub fn eat_at_restaurant() {
+    hosting::add_to_waitlist();
+     // error: hosting::some_function();
+}
+
+ // use: function and same_name's others always use to his nearest father module
+    // but struct, enum, and other items use to his top father module
--- use_trying/src/main.rs
+++ use_trying/src/main.rs
@@ -1,3 +1,18 @@
+use std::fmt;
+use std::io;
+ /*
+    use std::fmt::Result;
+    use std::io::Result as IoResult;
+    fn f2() -> IoResult<()> {
+        Ok(())
+    }
+ */
+fn  f1() -> fmt::Result {
+    Ok(())
+}
+fn f2() -> io::Result<()> {
+    Ok(())
+}
+
 fn main() {
-    println!("Hello, world!");
 }
