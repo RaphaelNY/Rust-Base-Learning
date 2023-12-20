8638ea660b9ab2a122fec14f69efece2dc76de90
RaphaelNY <1278173058@qq.com>
2023-12-20 15:29:31 +0800

    update about use other package

--- use_trying/src/main.rs
+++ use_trying/src/main.rs
@@ -1,5 +1,5 @@
 use std::fmt;
-use std::io;
+ // use std::io;
  /*
     use std::fmt::Result;
     use std::io::Result as IoResult;
@@ -17,5 +17,13 @@ fn f2() -> io::Result<()> {
     Ok(())
 }
 
+use rand::Rng;
+use std::collections::HashMap;
+////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
+ // use std::cmp::Ordering;
+use std::cmp::Ordering::*; // load all of public's items from Ordering
+ // use std::{cmp::Ordering, io};
+use std::io::{self, Write};
 fn main() {
+
 }
