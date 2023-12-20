e5b41776eb16cd74efa2f84365fff51afa47abda
RaphaelNY <1278173058@qq.com>
2023-12-20 15:16:34 +0800

    update

--- use_trying/src/main.rs
+++ use_trying/src/main.rs
@@ -3,6 +3,9 @@ use std::io;
  /*
     use std::fmt::Result;
     use std::io::Result as IoResult;
+    fn f1() -> Result {
+        Ok(())
+    }
     fn f2() -> IoResult<()> {
         Ok(())
     }
