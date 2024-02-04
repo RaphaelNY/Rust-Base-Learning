a54507c9b48d6563f17c8d2e1e5c7557e0da6073
RaphaelNY <1278173058@qq.com>
2024-02-04 23:50:20 +0800

    complete 10.5 lifetime(3/4)

--- lifetime_trying/src/main.rs
+++ lifetime_trying/src/main.rs
@@ -14,6 +14,18 @@
  /// -'a mut i32   // a mutable reference with an explicit lifetime
  /// ## lifetime elision
  /// - signaled in function signature
+ /// ## lifetime in struct
+ /// ## rule of lifetime
+ /// there are three rule, if code didn't meet the rule, the code will not be compiled.
+ /// these rule is for "fn" and "impl" block.
+ /// - first, every input reference gets its own lifetime parameter.
+ /// - second, if there is exactly one input lifetime parameter, that lifetime is assigned to all output lifetime parameters.
+ /// - third, if there are multiple input lifetime parameters, but one of them is "&self" or "&mut self", the lifetime of self
+ ///                                                                         is assigned to all output lifetime parameters.
+
+ struct ImportantExcerpt<'a> {
+     part: &'a str,
+ }
 
 fn main() {
     {
@@ -23,6 +35,16 @@ fn main() {
             r = &x;                                           //   |      |
         } // x is dropped here,so r is a dangling reference.  // --+      |
         println!("r: {}", r);                                 // ---------+
+    }
+    {
+        let novel = String::from("Call me Ishmael. Some years ago...");
+        let first_sentence = novel.split('.')
+            .next()
+            .expect("Could not find a '.'");
+
+        let i = ImportantExcerpt {
+            part: first_sentence
+        };
     }
 }
 
