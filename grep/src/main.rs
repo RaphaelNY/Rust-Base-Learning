d9f788fd2ab76de0014c60b5b6f2de1f2ce6254d
RaphaelNY <1278173058@qq.com>
2024-02-08 23:56:36 +0800

    complete 12

--- grep/src/main.rs
+++ grep/src/main.rs
@@ -1,3 +1,21 @@
-fn main() {
-    println!("Hello, world!");
+use std::env; // args
+use std::process; // exit
+
+use grep::*;
+
+fn main(){
+    let args: Vec<String> = env::args().collect();
+     // env::args_os() //return type is OsString use this can get illegal char.
+
+    let config = Config::new(&args).unwrap_or_else(|err| {
+        eprintln!("Problem parsing arguments: {}", err);
+        process::exit(1);
+    });
+
+    if let Err(e) = run(config) {
+        eprintln!("Application error: {}", e);
+        process::exit(1);
+    };
 }
+
+
