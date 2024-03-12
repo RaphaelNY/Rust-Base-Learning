9c308d91cd5fe4bafde574a310691714373b51c6
RaphaelNY <1278173058@qq.com>
2024-03-12 16:21:12 +0800

    complete 13.4-6

--- closures_trying/src/lib.rs
+++ closures_trying/src/lib.rs
@@ -1,13 +1,15 @@
 use std::thread;
 use std::time::Duration;
+use std::collections::HashMap;
 
 
+ // used Hashmap to rewrite the Cacher struct,makes it can be used for different income.
 struct Cacher<T>
 where
     T: Fn(u32) -> u32,
 {
     calculation: T,
-    value: Option<u32>,
+    value: HashMap<u32, u32>,
 }impl<T> Cacher<T>
 where
     T: Fn(u32) -> u32,
@@ -15,16 +17,17 @@ where
     fn new(calculation: T) -> Cacher<T> {
         Cacher {
             calculation,
-            value: None,
+            value: HashMap::new(),
         }
     }
 
     fn value(&mut self, arg: u32) -> u32 {
-        match self.value {
-            Some(v) => v,
+        let x = self.value.get(&arg);
+        match self.value.get(&arg) { // the value is a Option, so need to use * to get the value. self.value.get(&arg) return a Option<&T>,
+            Some(v) => *v, // Some(&T) => v :&u32, *v :u32
             None => {
                 let v = (self.calculation)(arg);
-                self.value = Some(v);
+                self.value.insert(arg, v);
                 v
             }
         }
@@ -67,10 +70,41 @@ mod tests {
     fn call_with_different_values() {
         let mut c = Cacher::new(|a| a);
 
-        let v1 = c.value(1);
+        let _v1 = c.value(1);
         let v2 = c.value(2);
          // in the second or more times, the value is not calculated again,will keep the value from first used.
 
         assert_eq!(v2, 2);
+    }
+
+    #[test]
+    fn iterator_demonstration() {
+        let v1 = vec![1, 2, 3];
+        let mut v1_iter = v1.iter();
+
+        // for val in v1_iter {
+        //    println!("Got: {}", val);
+        //}
+
+        assert_eq!(v1_iter.next(), Some(&1)); // it will change v1_iter to the next position.so v1_iter need be mutable.
+        assert_eq!(v1_iter.next(), Some(&2));
+        assert_eq!(v1_iter.next(), Some(&3));
+    }
+
+    #[test]
+    fn iterator_sum() {
+        let v1 = vec![1, 2, 3];
+        let v1_iter = v1.iter();
+
+        let total: i32 = v1_iter.sum();
+
+        assert_eq!(total, 6);
+    }
+
+    #[test]
+    fn iterator_sum_2() {
+        let v1 = vec![1, 2, 3];
+         // v1.iter().map(|x| x + 1); // it doesn't do anything, because the iterator is lazy, means it won't do anything until it's used.and what in v1 will not change.
+        let v2 :Vec<_> = v1.iter().map(|x| x + 1).collect();
     }
 }
--- closures_trying/src/main.rs
+++ closures_trying/src/main.rs
@@ -16,6 +16,33 @@ use closures_trying::*;
 /// - create a struct whitch has closures and it working result
 /// - cacher's limited
 ///     - only accept one closure
+/// ## 13.4 closures can visit the environment, can visit all varies in the environment where it's defined
+/// - how to get the value from the environment?
+///     - get the ownership: FnOnce
+///     - get the mutable reference: FnMut
+///     - get the immutable reference: Fn
+/// - when the closure first be used, the type & Trait of the closure is inferred. FnOnce{FnMut{Fn}}
+///     - all closures implement FnOnce
+///     - if the closure didn't move the environment, it will implement FnMut
+///     - if the closure didn't change the environment, it will implement Fn
+/// - used move can ordered closures must get the ownership of the environment
+/// ## 13.5 iterators
+/// in rust, iterator is lazy, means it won't do anything until it's used.
+/// - iterator trait && next method
+///     // pub trait Iterator {
+///     //    type Item;
+///     //    fn next(&mut self) -> Option<Self::Item>;
+///     //  // methods with default implementations elided
+///     // }
+/// - type Item and Self::Item are associated types, means they are defined in the trait, but the implementer will define the concrete type.
+/// - iterator only ordered next method.
+///     - next: once return a Option, covered in Some(Item) or None
+///         - Some(Item): the next value
+///         - None: the iterator is over
+///     when iterator next out a item, it will move the iterator to the next position.
+/// ## 13.6 used iterator
+/// - map: receive a closure, return a new iterator, the new iterator will cover the value that the closure return.
+///
 fn main() {
     let simulated_user_specified_value = 10;
     let simulated_random_number = 7;
@@ -23,6 +50,30 @@ fn main() {
     generate_workout(simulated_user_specified_value, simulated_random_number);
 ////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
     let example_closure = |x| x;
-    let s = example_closure(String::from("hello")); // when closures first be used, the type of the closure is inferred
+    let _s = example_closure(String::from("hello")); // when closures first be used, the type of the closure is inferred
      // error: let n = example_closure(5);
+////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
+    let x :i32 = 4;
+    let equal_to_x = |z| z == x; // the closure can visit the environment where it's defined
+     // fn equal_to_x(z: i32) -> bool {z == x} // error: the function can't visit the environment where it's defined,means it can't catch x.
+    let y :i32 = 4;
+    assert!(equal_to_x(y));
+////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
+     // 13.4
+    let x = vec![1, 2, 3];
+    let equal_to_x = move |z| z == x; // the closure can visit the environment where it's defined
+    // println!("can't use x here: {:?}", x); // error: the closure get the ownership of the environment
+    let y = vec![1, 2, 3];
+    assert!(equal_to_x(y));
+////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
+     // 13.5 iterators
+    let v1 = vec![1, 2, 3];
+    let v1_iter = v1.iter(); // the iterator is lazy, means it won't do anything until it's used.
+
+    for val in v1_iter { // iterator:v1_iter was moved here, so it can't be used again.
+        println!("Got: {}", val);
+    }
+////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
+     // iterator trait && next method
+
 }
