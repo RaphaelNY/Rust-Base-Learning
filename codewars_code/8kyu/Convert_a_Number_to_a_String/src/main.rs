94177370baf7c708f7701ed305645af7b5ccf4ea
RaphaelNY <1278173058@qq.com>
2024-04-09 21:12:17 +0800

    complete sum mixed array

--- codewars_code/8kyu/Convert_a_Number_to_a_String/src/main.rs
+++ codewars_code/8kyu/Convert_a_Number_to_a_String/src/main.rs
@@ -1,3 +1,6 @@
+
+use either::Either;
+
 fn main() {
     println!("Convert a Number to a String!");
 }
@@ -9,6 +12,14 @@ fn number_to_string(i: i32) -> String {
 fn string_to_number(s: &str) -> i32 {
     s.parse().unwrap()
     // s.parse::<i32>().unwrap()
+}
+
+fn sum_mix(arr: &[Either<i32, String>]) -> i32 {
+    arr.iter().map(|x| match x {
+        Either::Left(i) => *i,
+        Either::Right(s) => s.parse::<i32>().unwrap()
+    }).sum()
+    // arr.iter().cloned().map(|e| e.left_or_else(|x| x.parse::<i32>().unwrap())).sum()
 }
 
 #[cfg(test)]
@@ -47,5 +58,22 @@ mod tests {
             let input = num.to_string();
             assert_eq!(string_to_number(&input), num);
         }
+    }
+
+    use super::sum_mix;
+    use either::Either;
+
+    fn dotest2(arr: &[Either<i32, String>], expected: i32) {
+        let actual = sum_mix(arr);
+        assert!(actual == expected, "With arr = {arr:?}\nExpected {expected} but got {actual}")
+    }
+
+    #[test]
+    fn fixed_tests2() {
+        dotest2(&[Either::Left(9), Either::Left(3), Either::Right("7".to_string()), Either::Right("3".to_string())], 22);
+        dotest2(&[Either::Right("5".to_string()), Either::Right("0".to_string().to_string()), Either::Left(9), Either::Left(3), Either::Left(2), Either::Left(1), Either::Right("9".to_string()), Either::Left(6), Either::Left(7)], 42);
+        dotest2(&[Either::Right("3".to_string()), Either::Left(6), Either::Left(6), Either::Left(0), Either::Right("5".to_string()), Either::Left(8), Either::Left(5), Either::Right("6".to_string()), Either::Left(2), Either::Right("0".to_string())], 41);
+        dotest2(&[Either::Right("1".to_string()), Either::Right("5".to_string()), Either::Right("8".to_string()), Either::Left(8), Either::Left(9), Either::Left(9), Either::Left(2), Either::Right("3".to_string())], 45);
+        dotest2(&[Either::Left(8), Either::Left(0), Either::Left(0), Either::Left(8), Either::Left(5), Either::Left(7), Either::Left(2), Either::Left(3), Either::Left(7), Either::Left(8), Either::Left(6), Either::Left(7)], 61);
     }
 }
